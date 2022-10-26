b7bbf96f0d8b0cf37b74649f699bcc52521d1087
deemasss <dimon.limon.s90@gmail.com>
2022-10-27 02:33:49 +0300

    Add files

--- src/User.js
+++ src/User.js
@@ -33,7 +33,7 @@ export function User(name, date) {
     this.likeBook = function(like) {
         if (this.likes.includes(like)) {
             this.likes = this.likes.filter((book) => book !== like);
-            book.likedUsers = book.likedUsers.filter((book) => book !== this);
+            like.likedUsers = like.likedUsers.filter((book) => book !== this);
             return;
         }
         this.likes.push(like)
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import { User } from './User.js';
 import { Book } from './Book.js';
 import { Author } from './Author.js';
-
+/*
 const dima = new User ('Dimon', new Date());
 const alina = new User ('Alina', new Date());
 const bohdan = new User ('Bohdan', new Date());
-
-
 
 const user = new User('John', new Date(2020, 1, 1));
 const anotherUser = new User('Jerry', new Date(2020, 1, 1));
@@ -21,7 +19,8 @@ const book333 = new Book('333', new Date(), dima, authors);
 
 dima.addToFriends(alina);
 dima.addToFriends(bohdan);
-//dima.likeBook(book1984);
+
+dima.likeBook(book1984);
 
 
 console.log(dima.likedBooks);
@@ -30,4 +29,5 @@ console.log(dima.friendsNames);
 
 console.log(book1984.suggestedPublicators);
 console.log(book1984.suggestedBooks);
-
+console.log(book1984);
+*/
